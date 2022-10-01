6d7e19e761c2394fd49f9f7f93348e69f0aceb4c
situodys <situodys@gmail.com>
2022-10-01 19:41:40 +0900

    221001

---
+++ lv2/기능개발.java
@@ -0,0 +1,43 @@
+package programmers.lv2;
+import java.util.*;
+
+public class 기능개발 {
+    public int[] solution(int[] progresses, int[] speeds) {
+        int[] answer = {};
+
+        Queue<Integer> arr = new LinkedList<>();
+        ArrayList<Integer> tmp = new ArrayList<>();
+
+        int diff=0;
+        for(int i=0;i<progresses.length;i++){
+            diff = 100 -progresses[i];
+            arr.offer(diff%speeds[i] == 0 ? diff/speeds[i] : diff/speeds[i]+1);
+        }
+
+
+        int cur = arr.poll();
+        int nxt=0;
+        int cnt=1;
+
+        while(!arr.isEmpty()){
+            nxt = arr.poll();
+            if(cur < nxt){
+                tmp.add(cnt);
+                cnt=1;
+                cur =nxt;
+                continue;
+            }
+            cnt++;
+        }
+        tmp.add(cnt);
+
+        answer = new int[tmp.size()];
+
+        for(int i=0;i<tmp.size();i++){
+            answer[i]= tmp.get(i);
+        }
+
+
+        return answer;
+    }
+}
