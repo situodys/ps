eb7589b07df34402c5708772f28b36d6b043db70
situodys <situodys@gmail.com>
2022-10-03 18:48:36 +0900

    221003

---
+++ lv2/전화번호_목록.java
@@ -0,0 +1,32 @@
+package programmers.lv2;
+import java.util.*;
+
+public class 전화번호_목록 {
+
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+
+        Set<String> st = new TreeSet<>();
+
+        for(int i=0;i<phone_book.length;i++){
+            st.add(phone_book[i]);
+        }
+
+        Iterator<String> it = st.iterator();
+        String cur;
+        String next;
+
+        cur = it.next();
+
+        while(it.hasNext()){
+            next = it.next();
+            if(next.startsWith(cur)){
+                answer=false;
+                break;
+            }
+            cur=next;
+        }
+
+        return answer;
+    }
+}
