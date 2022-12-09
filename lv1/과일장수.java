df9f4f0b88d1e2373a1de178891be557ed516433
situodys <situodys@gmail.com>
2022-12-09 16:29:28 +0900

    221209

---
+++ lv1/과일장수.java
@@ -0,0 +1,18 @@
+package programmers.lv1;
+
+import java.util.*;
+
+public class 과일장수 {
+
+    public int solution(int k, int m, int[] score) {
+        int ans = 0 ;
+        Arrays.sort(score);
+
+        for (int i = score.length - 1; i >= 0; i--) {
+            if ((score.length - i) % m == 0) {
+                ans += score[i] * m;
+            }
+        }
+        return ans;
+    }
+}
