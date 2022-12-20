5c43668bc0e02c7e264e1cc4032063e63851fbcc
situodys <situodys@gmail.com>
2022-12-20 10:45:08 +0900

    221220

---
+++ lv2/귤고르기.java
@@ -0,0 +1,26 @@
+package programmers.lv2;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class 귤고르기 {
+    public static Map<Integer,Integer> countPerItem = new HashMap<>();
+    public int solution(int k, int[] tangerine) {
+        int answer = 0;
+
+        for(int value : tangerine){
+            countPerItem.put(value,countPerItem.getOrDefault(value,0)+1);
+        }
+
+        List<Integer> lst = countPerItem.values().stream().sorted(Collections.reverseOrder()).collect(Collectors.toList());
+
+        for(int val : lst){
+            if(k<=0) return answer;
+            k-=val;
+            answer++;
+        }
+
+
+        return answer;
+    }
+}
