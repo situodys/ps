c4b211ca85ead2b4b2d44df18cd63e9e296cc33b
situodys <situodys@gmail.com>
2023-01-02 00:29:07 +0900

    230102

---
+++ lv1/명예의_전당1.java
@@ -0,0 +1,19 @@
+package programmers.lv1;
+
+import java.util.PriorityQueue;
+
+public class 명예의_전당1 {
+    public int[] solution(int k, int[] score) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int[] answer = new int[score.length];
+
+        for(int i=0; i<score.length; i++){
+            pq.add(score[i]);
+            if(pq.size()>k)
+                pq.poll();
+            answer[i]= pq.peek();
+        }
+
+        return answer;
+    }
+}
